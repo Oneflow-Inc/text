83b428d3664f6f571d7e77dfe71b5bdd323746dd
xiezipeng <xzpaiks@163.com>
2022-03-11 15:33:42 +0800

    add infer_shape function

---
+++ flowtext/vocab/vectors.py
@@ -0,0 +1,31 @@
+import gzip
+import logging
+import os
+import tarfile
+import zipfile
+from urllib.request import urlretrieve
+
+import oneflow as flow
+from tqdm import tqdm
+
+from ..utils import reporthook
+
+logger = logging.getLogger(__name__)
+
+
+def _infer_shape(f):
+    num_lines, vector_dim = 0, None
+    for line in f:
+        if vector_dim is None:
+            row = line.rstrip().split(b" ")
+            vector = row[1:]
+            # Assuming word, [vector] format
+            if len(vector) > 2:
+                # The header present in some (w2v) formats contains two elements.
+                vector_dim = len(vector)
+                num_lines += 1  # First element read
+        else:
+            num_lines += 1
+    f.seek(0)
+    return num_lines, vector_dim
+
