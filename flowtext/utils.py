a4e9b84711e456fba2c5e1562c7863bc0547c790
Zipeng Xie <53039617+xiezipeng-ML@users.noreply.github.com>
2022-03-12 20:10:28 +0800

    Dev xzp add vocab (#14)
    * add reporthook function
    
    * add infer_shape function
    
    * add Vectors
    
    * add GloVe vectors
    
    * add FastText vectors
    
    * add CharNGram vectors
    
    * add pretrined_aliases

--- flowtext/utils.py
+++ flowtext/utils.py
@@ -10,6 +10,29 @@ import gzip
 from ._download_datasets import _DATASET_DOWNLOAD_MANAGER
 
 
+def reporthook(t):
+    """
+    https://github.com/tqdm/tqdm.
+    """
+    last_b = [0]
+
+    def inner(b=1, bsize=1, tsize=None):
+        """
+        b: int, optional
+        Number of blocks just transferred [default: 1].
+        bsize: int, optional
+        Size of each block (in tqdm units) [default: 1].
+        tsize: int, optional
+        Total size (in tqdm units). If [default: None] remains unchanged.
+        """
+        if tsize is not None:
+            t.total = tsize
+        t.update((b - last_b[0]) * bsize)
+        last_b[0] = b
+        
+    return inner
+
+
 def download_from_url(url, path=None, root=".data"):
     """
     Args:
