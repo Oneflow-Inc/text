8aabaa50abeb622ea969a5142fddab8fe2f15119
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-29 13:07:32 +0000

    Adding tarfile member sanitization to extractall()

--- flowtext/models/elmo/utils.py
+++ flowtext/models/elmo/utils.py
@@ -251,7 +251,26 @@ def load_state_dict_from_url(url: str, saved_path: str):
             )
         )
         with tarfile.open(package_path) as f:
-            f.extractall(saved_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, saved_path)
     file_name = url_parse.path.split("/")[-1].split(".")[0]
     file_path = os.path.join(saved_path, file_name)
     return file_path
--- flowtext/models/utils.py
+++ flowtext/models/utils.py
@@ -29,7 +29,26 @@ def load_state_dict_from_url(url: str, saved_path: str):
                 )
             )
             with tarfile.open(package_path) as f:
-                f.extractall(saved_path)
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(f, saved_path)
     cpt = get_cpt(file_path)
     config_file = os.path.join(file_path, "config.json")
     vocab_file = os.path.join(file_path, "vocab.txt")
